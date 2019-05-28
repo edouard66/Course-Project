ed48b3f4118563161b6cb587196caf1656264292
edouard66 <50629062+edouard66@users.noreply.github.com>
2019-05-28 14:26:10 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,7 @@
 run_analysis <- function() {
       library(dplyr)
   
-## 1st step : let's download the zip file from the website and store the data locally.
+## **1st step** : let's download the zip file from the website and store the data locally.
 ## We create two seperate tables for the test subjects and the train subjects.
 ## After adding subjects and activity to both datasets using the cbind function, we merge them in a single dataset using the rbind function.
 ## Note that we also added  an extra "group" variable to remind us if the subject was part of the testing or training group.
@@ -43,7 +43,7 @@ run_analysis <- function() {
       X <- rbind(X_test, X_train)
       print("Test and train data merged into dataset X")
 
-## 2nd step : let's import the file with variable labels and use it to appropriately label the data set with descriptive variable names.
+## **2nd step** : let's import the file with variable labels and use it to appropriately label the data set with descriptive variable names.
 ## This step correspond to requirement 4 in the Coursera assignment.
       
       featuresPath <- "./UCI HAR Dataset/features.txt"
@@ -55,7 +55,7 @@ run_analysis <- function() {
       colnames(X) <- features[[2]]
       print("Variable names successfully added")
       
-## 3rd step : let's use descriptive activity names to name the activities in the data set. 
+## **3rd step** : let's use descriptive activity names to name the activities in the data set. 
 ## To do so, we import the "activity_labels" file.
 ## And we use a loop that reviews each row of the dataset "X" and finds the corresponding activity label. 
 ## This step correspond to requirement 3 in the Coursera assignment.
@@ -75,14 +75,14 @@ run_analysis <- function() {
       print("X preview :")
       print(str(X))
       
-## 4th step : Let's extract only the measurements on the mean and standard deviation for each measurement.
+## **4th step** : Let's extract only the measurements on the mean and standard deviation for each measurement.
 ## We use a regexp to select variables using the Mean() or Std() functions.
 ## This step correspond to requirement 2 in the Coursera assignment.
       
       ms_regexp <- grep("(|)[Mm]ean\\()|[Ss]td\\()|activity|group|subject",colnames(X), value = TRUE)
       X_mean_std <- X[, ms_regexp]
       
-## 5th step : From the data set in step 4, we create a second, independent tidy data set named "finalX" with the average of each variable for each activity and each subject.
+## **5th step** : From the data set in step 4, we create a second, independent tidy data set named "finalX" with the average of each variable for each activity and each subject.
 ## This step correspond to requirement 5 in the Coursera assignment.
       
       finalX <- group_by(X_mean_std,group, subject, activity)
